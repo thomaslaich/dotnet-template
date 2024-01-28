a2310ccb720fad7b341a86e3bc222dec4562e96f
Thomas Laich <thomaslaich@gmail.com>
2024-01-28 18:22:43 +0100

    update treefmt config to use built-in csharpier

--- treefmt.nix
+++ treefmt.nix
@@ -1,16 +1,5 @@
 { pkgs, lib, ... }: {
   projectRootFile = "flake.nix";
   programs.nixfmt.enable = true;
-  settings.formatter = {
-    "csharpier" = {
-      command = pkgs.writeShellApplication {
-        name = "csharpier-fix";
-        runtimeInputs = with pkgs; [ dotnet-sdk csharpier ];
-        text = ''
-          dotnet-csharpier "$@"
-        '';
-      };
-      includes = [ "*.cs" ];
-    };
-  };
+  programs.csharpier.enable = true;
 }
